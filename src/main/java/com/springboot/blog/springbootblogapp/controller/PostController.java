c8cbc642fa053bc9f80d088cb2ab8d62d858053c
vijay <sarojvijay1971998@gmail.com>
2024-01-02 23:00:39 +0530

    this is used to security validation

--- src/main/java/com/springboot/blog/springbootblogapp/controller/PostController.java
+++ src/main/java/com/springboot/blog/springbootblogapp/controller/PostController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -32,6 +33,7 @@ public class PostController {
 	}
 	
 	//postMapping 
+	@PreAuthorize("hasRole('ADMIN')")
 	@PostMapping
 	public ResponseEntity<PostDto> createPost(@Valid @RequestBody PostDto postDto){
 		
@@ -53,6 +55,7 @@ public class PostController {
 		return ResponseEntity.ok(postService.getPostById(id));
 	}
 	
+	@PreAuthorize("hasRole('ADMIN')")
 	@PutMapping("/{id}")
 	public ResponseEntity<PostDto> updatePost(@Valid @RequestBody PostDto postDto,@PathVariable("id") long id){
 		
@@ -60,6 +63,7 @@ public class PostController {
 		return new ResponseEntity<>(postResponse,HttpStatus.OK);
 	}
 	
+	@PreAuthorize("hasRole('ADMIN')")
 	@DeleteMapping("{id}")
 	public ResponseEntity<String> deletePost(@PathVariable("id") long id) {
 		
